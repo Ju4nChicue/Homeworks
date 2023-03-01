cd02baf25f0e715e096f6aedacb1653a3f38a7c5
JuanChicue <juan.chicue@uao.edu.co>
2023-03-01 01:42:28 -0500

    Challenge_3

--- Challenges/src/home.jsx
+++ Challenges/src/home.jsx
@@ -1,38 +1,11 @@
-import { useState } from 'react';
 import './App.css'
-import { FirstApp, Counter, GifExpert, GifGrid, Counter2, Fetch_component, BreakingBad } from './'
+import { GifExpert } from './Components/GifExpert' 
 
 const Home = () =>{
 
-    const components = [<FirstApp />, <Counter value={0} />, <GifExpert />, <GifGrid category={"algo"} />, <Counter2 value={0}/>, <Fetch_component />, <BreakingBad />]
-
-    const [page, setPage] = useState(0)
-
-    const handleAdd = () => {
-        if (page < components.length - 1){
-            setPage( page + 1)
-        }
-    }
-
-    const handleSubsstract = () => {
-        if (page > 0){
-            setPage ( page - 1)
-        }
-    }
-
     return (
         <>
-            <div className='Select'>
-                <h1>Challenge {page + 1}</h1>
-                <button onClick={() => handleSubsstract()}> Anterior </button>
-                <button onClick={() => handleAdd()}> Siguiente </button>
-            </div>
-            
-            <div className='challenges'>
-                {
-                    components[page]
-                }
-            </div>
+            <GifExpert />
         </>
       )
 }
