c6cb945dc461493f05e0b9a3e14c9afa9d9b68e6
Juan Chicue <jjcc020@gmail.com>
2023-04-17 00:29:23 -0500

    Challenges hasta el 10, tema hasta context. Antes de intalar router

---
+++ Challenges/src/components/contextExample/Child2.jsx
@@ -0,0 +1,27 @@
+import React, { useContext } from "react";
+import { UserContext } from "../../context/UserContext";
+
+export const Child2 = () => {
+
+    const { setUser } = useContext(UserContext);
+
+    const onLogin = () => {
+        setUser({
+            id: 123,
+            name: "Juan",
+            email: "juan@prueba.com"
+        });
+    }
+
+    return (
+        <>
+            <div>Child Component 2</div>
+            <button
+                className="btn btn-primary"
+                onClick={onLogin}
+            >
+                set User Info
+            </button>
+        </>
+    )
+}
--- Challenges/src/main.jsx
+++ Challenges/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import Home from './home'
+//import './reducer/intro-reducer'
 
 /**const [page, setPage] = setState( value )
 const [componets, setComponents] = useState([])**/
@@ -9,5 +10,5 @@ const [componets, setComponents] = useState([])**/
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Home />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
